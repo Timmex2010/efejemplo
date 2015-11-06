55c09e3f2149fd034cd36aa06a8f9d854a542b50
Leong <timmex2010@gmail.com>
2015-11-06 08:20:10 -0800

    se agregaron deps

---
+++ MiDb/DemoEF.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01.MiDb
+{
+    public class DemoEF : Dbcontext
+    {
+        DbSet<Empleado> Empleados { get; set; }
+        //DbSet<Autos> Empleados { get; set; }
+    }
+}
--- mibd/demoEF.cs
+++ mibd/demoEF.cs
@@ -10,6 +10,6 @@ namespace Ejercicio01.mibd
     public class demoEF :DbContext
     {
        public DbSet<Empleado> Empleados {get; set;}//instancia para crear tablas dentro de la base de datos
-
+           public DbSet<Departamento> Departamento { set; get; }
     }
 }
